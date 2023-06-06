6e377a88b08744c1df1b0663266517eb0c5b596d
theenduranceA <theenduranceaneke@gmail.com>
2023-06-06 16:09:13 -0700

    Fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for num1 in range(10):
+for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
-        print("{:02d}".format(num1 * 10 + num2), end=", ")
-print("{:02d}".format(99))
+        if num1 == 8 and num2 == 9:
+            print("{:d}{:d}".format(num1, num2))
+        else:
+            print("{:d}{:d}".format(num1, num2), end=", ")
