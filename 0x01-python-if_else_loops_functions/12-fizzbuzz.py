382e2599b5fd752cfec9eee82d24331d2f828902
theenduranceA <theenduranceaneke@gmail.com>
2023-06-06 18:04:55 -0700

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,5 +7,4 @@ def fizzbuzz():
             print("Buzz", end="")
         if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz", end="")
-        else:
-            print(num, end="")
+        print(num, end="")
