459fb2d82e20e160144466f5c29acbc7bac4c5bf
theenduranceA <theenduranceaneke@gmail.com>
2023-06-08 15:36:20 -0700

    Task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for num in range(4, 6):
-            c = add(c, i)
+            c = add(c, num)
         return c
     else:
         return sub(a, b)
