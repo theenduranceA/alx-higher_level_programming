34b28151dea10475b32a068baa9611de83ea4d4f
theenduranceA <theenduranceaneke@gmail.com>
2023-06-14 17:53:17 -0700

    Task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda y: y ** 2, y)), matrix))
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
