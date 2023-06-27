02379790226c0894880e55068389dc3dfa428416
theenduranceA <theenduranceaneke@gmail.com>
2023-06-27 13:52:59 -0700

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A class defining a square."""
+
+
+class Square():
+    """A square class."""
+
+    def __init__(self, size=0):
+        """Initializating the class square."""
+
+        self.__size = size
+        """Size is a private attribute."""
+
+    def area(self):
+        """Public instance method, returns the current area of the Square."""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """To retrieve the size of the Square."""
+        return self.__size
+
+    @size.setter
+    def size(self, SizeValue):
+        """To set the size of the Square."""
+
+        if type(SizeValue) != int:
+            raise TypeError("size must be an integer")
+        if SizeValue < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = SizeValue
