1de5d60f681afbd38f01466c01ee8dfe10943543
theenduranceA <theenduranceaneke@gmail.com>
2023-06-27 13:21:30 -0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A class defining a square."""
+
+
+class Square():
+    """A square class."""
+
+    def __init__(self, size=0):
+        """Initializating the class square."""
+
+        if type(size) != int:
+            """Size is a private instance attribute."""
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
