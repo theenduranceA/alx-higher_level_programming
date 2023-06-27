adfdec76488e91004814139763391d7ac19a4e80
theenduranceA <theenduranceaneke@gmail.com>
2023-06-27 12:56:09 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square():
+    """A square class."""
+
+    def __init__(self, size):
+        """Initializating the class square."""
+        self.__size = size
