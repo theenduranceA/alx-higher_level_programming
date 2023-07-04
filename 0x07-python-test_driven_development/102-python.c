e607499b98324d2215b457e2631ddc423fc957de
theenduranceA <theenduranceaneke@gmail.com>
2023-07-04 17:44:26 +0000

    Task 8

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include "Python.h"
+#include <Python.h>
 
-/**
- * print_python_string - Prints string information
- *
- * @p: Python Object
- * Return: no return
- */
 void print_python_string(PyObject *p)
 {
-
-	PyObject *str, *repr;
-
-	(void)repr;
 	printf("[.] string object info\n");
 
-	if (strcmp(p->ob_type->tp_name, "str"))
+	if (!PyUnicode_Check(p))
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
+	if (PyUnicode_IS_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
 
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+	printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
+	printf("  value: %s\n", PyUnicode_AsUTF8(p));
 }
