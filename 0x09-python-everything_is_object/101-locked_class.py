06bb98aa5b6be93a8422ef02b9caac9399b983c6
theenduranceA <theenduranceaneke@gmail.com>
2023-07-04 04:55:37 -0700

    Task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A modeule for LockedClass"""
+
+
+class LockedClass:
+    """
+    A class with no class or object attribute,
+    that prevents the user from dynamically creating
+    new instance attributes,
+    except if the new instance attribute is called first_name
+    """
+    __slots__ = ["first_name"]
