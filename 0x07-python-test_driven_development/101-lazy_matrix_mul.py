efdb51ed67aff4d25a9e11a53884a118e45b4b51
theenduranceA <theenduranceaneke@gmail.com>
2023-07-04 09:53:12 -0700

    Task 7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Numpy LazyMatrix Module."""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function that multiplies 2 matrices by using numpy."""
+    return np.matmul(m_a, m_b)
