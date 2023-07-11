97abc1f98598e7442e0f489400b900079a1a67c7
theenduranceA <theenduranceaneke@gmail.com>
2023-07-10 17:55:59 -0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Module for is_kind_of_class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that returns true if the object is an instance of a class"""
+    return isinstance(obj, a_class)
