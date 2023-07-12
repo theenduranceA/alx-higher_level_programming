a035258d3ebb26ec85b49e820da719a761e27f18
theenduranceA <theenduranceaneke@gmail.com>
2023-07-11 21:44:27 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """Function that reads a text file"""
-    with open(filename, 'r') as f:
-        read_data = f.read()
-    print(read_data, end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end='')
