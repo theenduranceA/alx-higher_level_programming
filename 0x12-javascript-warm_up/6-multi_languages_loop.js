80db662ee1bca494edc7eeaf3801161c9e79782c
theenduranceA <theenduranceaneke@gmail.com>
2023-09-11 16:10:44 -0700

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/* Script that prints 3 lines by using an array of strings and a loop */
+
+const Myarray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; Myarray[i]; i++) {
+  console.log(Myarray[i]);
+}
