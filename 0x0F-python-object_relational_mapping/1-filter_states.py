2eb4bc13b7c449c5f0f1efab29d7ab3c841ddbb7
theenduranceA <theenduranceaneke@gmail.com>
2023-09-15 07:54:20 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa."""
+"""
+Script that lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 from sys import argv
