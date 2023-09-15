20d14832ff0cd3086fee8c0520039ca5dea69e32
theenduranceA <theenduranceaneke@gmail.com>
2023-09-15 09:28:04 -0700

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Module of class definition of a State and an instance."""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
+    """Class instance."""
+
     __tablename__ = "states"
 
     id = Column(
