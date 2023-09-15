94b2120fae78c027981c0e8ebd4247ac255c7fbc
theenduranceA <theenduranceaneke@gmail.com>
2023-09-15 12:43:00 -0700

    Task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -17,5 +17,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State).filter(City.state_id == State.id):
+    for city, state in session.query(City, State).\
+            filter(City.state_id == State.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
