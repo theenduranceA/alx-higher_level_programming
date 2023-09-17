97261b227f3123a19c64fed20374893cb0d3ae94
theenduranceA <theenduranceaneke@gmail.com>
2023-09-17 16:38:48 -0700

    Task 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -10,15 +10,14 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    st = session.query(State).join(City).order_by(City.id).all()
+    states = session.query(State).join(City).order_by(City.id).all()
 
-    for state in st:
+    for state in states:
         for city in state.cities:
             print("{}: {} -> {}".format(city.id, city.name, state.name))
