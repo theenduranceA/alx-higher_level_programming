881312823bb56f06f0684e970b2f4f8a8a796bcb
theenduranceA <theenduranceaneke@gmail.com>
2023-09-17 08:13:05 -0700

    Task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,8 +3,8 @@
 City objects, contained in the database hbtn_0e_14_usa."""
 
 from sys import argv
-from model_city import City
-from model_state import Base, State
+from relationship_city import City
+from relationship_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for data in session.query(State).order_by(State.id):
-        print("{}: {}".format(data.id, data.name))
-        for city in data.cities:
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
