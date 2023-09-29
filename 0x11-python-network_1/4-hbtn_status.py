bc0911d710afa01e3e023d127417327b7e2d1add
theenduranceA <theenduranceaneke@gmail.com>
2023-09-29 11:54:10 -0700

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    html = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")5
+    print("\t- type: {}".format(type(html.text)))
+    print("\t- content: {}".format(html.text))
