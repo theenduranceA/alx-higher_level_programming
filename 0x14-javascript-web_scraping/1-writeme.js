013acea6a693e3c5c0b7f486dd2de92a5920ee84
theenduranceA <theenduranceaneke@gmail.com>
2023-10-24 14:16:44 -0700

    Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Script that writes a string to a file. */
+
+const fs = require('fs');
+
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.appendFile(file, string, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
