35ee423c3774b4d895facf26c8fb6c81d6ae74f2
theenduranceA <theenduranceaneke@gmail.com>
2023-10-24 15:53:25 -0700

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,24 +2,13 @@
 /* Script that prints the title of a Star Wars movie where the episode number matches a given integer. */
 
 const request = require('request');
-
 const movieId = process.argv[2];
-
-if (!movieId) {
-  process.exit(1);
-}
-
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request.get(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    try {
-      const data = JSON.parse(body);
-      console.log(data.title);
-    } catch (parseError) {
-      process.exit(1);
-    }
+request.get(url, (err, response, body) => {
+	if (err) {
+    console.log(err);
   } else {
-    process.exit(1);
+    console.log(JSON.parse(body).title);
   }
 });
